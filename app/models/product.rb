3460b4878e7e6352801bdd0ceb778c385200d5ca
JParrales <josedparrales@gmail.com>
2021-07-26 18:01:01 -0500

    Callbacks

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,4 +11,42 @@
 #  updated_at :datetime         not null
 #
 class Product < ApplicationRecord
+
+    #save
+    before_save :validate_product
+    after_save :send_notification
+    after_save :push_notification, if: :discount?
+
+    def total
+        self.price *3500
+    end
+
+    def discount?
+        self.total < 50000
+    end
+
+    private 
+    
+    def validate_product
+        puts "\n\n\n>>> Un nuevo producto será añadido a almacen"
+    end
+    
+    def total
+        self.price*3500
+    end
+
+    def send_notification
+        puts "\n\n\n>>> Un nuevo producto fue añadido a almacen: #{self.title} - $#{self.total} COP"
+    end
+
+    def send_notification
+        puts "\n\n\n>>> Un nuevo producto fue añadido a almacen: #{self.title} - $#{self.total} COP"
+    end
+
+    
+    def push_notification
+        puts "\n\n\n>>> Un nuevo producto en descuento, se encuentra disponible: #{self.title}"
+    end
+
+
 end
